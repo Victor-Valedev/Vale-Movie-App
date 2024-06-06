e712dfd07584e7abcff7fb2b67f0368bf71f1177
Victor-Valedev <victorvaledev@gmail.com>
2024-06-05 23:07:26 -0300

    Added themes and colors, created the main view, recyclerview item view, and created the package adapter, api and model

---
+++ app/src/main/java/com/victor/valemovie/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.victor.valemovie
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.victor.valemovie.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+}
