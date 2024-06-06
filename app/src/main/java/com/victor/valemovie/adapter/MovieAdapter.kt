9ed594032c39360247ebd6f9d0e356cfc03b58a3
Victor-Valedev <victorvaledev@gmail.com>
2024-06-05 23:41:37 -0300

    added recyclerview class MovieAdapter

---
+++ app/src/main/java/com/victor/valemovie/adapter/MovieAdapter.kt
@@ -0,0 +1,25 @@
+package com.victor.valemovie.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.victor.valemovie.databinding.ItemFilmeBinding
+
+class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+
+    inner class MovieViewHolder(binding: ItemFilmeBinding) : RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
