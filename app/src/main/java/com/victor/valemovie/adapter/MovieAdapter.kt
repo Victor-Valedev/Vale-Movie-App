c4034237ba3e6737d6a16150b9574a9ed6194c05
Victor-Valedev <victorvaledev@gmail.com>
2024-06-07 21:58:10 -0300

    inserted function -addList-, created onCreateViewHolder with layout inflate, OnBindViewHolder and getItemCount

--- app/src/main/java/com/victor/valemovie/adapter/MovieAdapter.kt
+++ app/src/main/java/com/victor/valemovie/adapter/MovieAdapter.kt
@@ -1,25 +1,50 @@
 package com.victor.valemovie.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.victor.valemovie.databinding.ItemFilmeBinding
 
 class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
 
-    inner class MovieViewHolder(binding: ItemFilmeBinding) : RecyclerView.ViewHolder(binding.root){
+    private var listMovie: MutableList<String> = mutableListOf()
+
+    fun addList(list: List<Movie>){
+
+    }
+
+    inner class MovieViewHolder(binding: ItemFilmeBinding)
+        : RecyclerView.ViewHolder(binding.root){
+
+        fun bind(movie: Movie){
+
+
+
+        }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        TODO("Not yet implemented")
-    }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemFilmeBinding.inflate(
+            layoutInflater,
+            parent,
+            false
+        )
+
+        return MovieViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
+        var filme = listMovie[position]
+        holder.bind(filme)
+
+    }
+
+    override fun getItemCount(): Int {
+        return listMovie.size
     }
 
 }
